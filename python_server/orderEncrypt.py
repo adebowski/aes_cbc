007b25313e1f9de79c28a4ea25b32fc538727450
adebowski <artek.debowski@gmail.com>
2022-08-21 21:39:09 +0200

    Added missing python file

---
+++ python_server/orderEncrypt.py
@@ -0,0 +1,88 @@
+import argparse
+from os import urandom
+from binascii import unhexlify as decode, hexlify as encode
+from random import random
+from traceback import print_last
+import binascii
+import asyncio
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from hashlib import md5
+
+from base64 import b64encode
+
+
+def setup_args():
+    p = argparse.ArgumentParser(description="Creates encrypted orders data")
+    p.add_argument(
+        "executable_file", metavar="FIRMWARE_FILE", type=argparse.FileType("rb")
+    )
+
+    args = p.parse_args()
+    return args
+
+
+def aes_sha256(plain_text, key, iv):
+    encrypted = AES.new(key=key, mode=AES.MODE_CBC, iv=iv).encrypt(plain_text)
+    return encrypted
+
+
+def encrypt(plain_text, key, iv):
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    print(cipher)
+    return b64encode(iv + cipher.encrypt(pad(plain_text), AES.block_size))
+
+
+class AESCipher:
+    def __init__(self, key):
+        password = key.encode("utf-8")
+        self.key = md5(password).digest()
+
+    def encrypt(self, data):
+        vector = get_random_bytes(AES.block_size)
+        encryption_cipher = AES.new(self.key, AES.MODE_CBC, vector)
+
+        encrypted = encryption_cipher.encrypt(pad(data, AES.block_size))
+        return vector + encrypted
+
+    def decrypt(self, data):
+        file_vector = data[: AES.block_size]
+        decryption_cipher = AES.new(self.key, AES.MODE_CBC, file_vector)
+        return unpad(decryption_cipher.decrypt(data[AES.block_size :]), AES.block_size)
+
+
+async def Callback(reader, writer):
+    enc_key = "XhtBfSVHzDmWweev"
+
+    while True:
+        try:
+            text = input("Type a message:\n").encode("utf-8")
+            encrypt = AESCipher(enc_key).encrypt(text)
+            print("encrypted: ", binascii.hexlify(encrypt))
+            writer.write(encrypt)
+        except:
+            writer.close()
+
+
+async def StartServer(port):
+    server = await asyncio.start_server(Callback, "", port)
+    async with server:
+        await server.serve_forever()
+
+
+def main():
+    # args = setup_args()
+    try:
+        asyncio.run(
+            StartServer(
+                8888,
+            )
+        )
+    except KeyboardInterrupt:
+        print("Cannot start server")
+        pass
+
+
+if __name__ == "__main__":
+    main()
